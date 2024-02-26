1f9297cb45768969c9ffa2ffb10e048bb3dc09cb
Sertaç Kıncal <sertackincal@hotmail.com>
2024-02-26 16:24:05 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 
     static boolean isPalindrome(int number) {
-        int temp = number, reverseNumber = 0, lastNumber;
+        int temp = number, reverseNumber = 0, lastNumber;      //reversNumber sayının tersenn ifadesi,last number son rakamını temsil eder
         while (temp != 0) {
             lastNumber = temp % 10;
             reverseNumber = (reverseNumber * 10) + lastNumber;
             temp /= 10;
         }
-        if (number == reverseNumber)
-        return true;
+        if (number == reverseNumber)     //sayının tersiyle eşitliği kontrol edilir 
+        return true;                      //sayı aynı ise true döner değilse false döner
         else
             return  false;
     }
